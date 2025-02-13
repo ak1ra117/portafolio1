7c78c7d881037464122c9cb807f73be20fcbdb87
ak1ra117 <114460375+ak1ra117@users.noreply.github.com>
2025-02-13 10:17:02 -0600

    Delete python directory

--- python/vpn_monitor.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-ip_address = "X.X.X.X"  # Replace with your Azure VPN IP
-
-for i in range(5):
-    response = os.popen(f"ping -c 1 {ip_address}").read()
-    print(response)
-    time.sleep(1)
